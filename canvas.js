3b10cb879504c1c71bb499e12e8245211f2a8276
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-06 13:14:05 -0600

    add random size and location for squares

--- canvas.js
+++ canvas.js
@@ -5,12 +5,52 @@ function drawBorder(){
   context.strokeRect(0,0, size.x, size.y);
 }
 
-function fillCanvas(){
+function fillBlue(){
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
   context.fillStyle = "blue";
-  context.fillRect(10, 10, 10, 40);
+  var size = { x: canvas.width, y: canvas.height };
+  context.fillRect(0, 0, size.x/2, size.y/2);
 }
 
+function fillRed(){
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  context.fillStyle = "red";
+  var size = { x: canvas.width, y: canvas.height };
+  context.fillRect(canvas.height/2, canvas.width/2, size.x/2, size.y/2);
+}
+
+function buttonOrange(){
+  var context = canvas.getContext('2d');
+  context.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+  context.globalAlpha = Math.random();
+  context.fillRect(Math.floor(Math.random()*200),Math.floor(Math.random()*100), Math.floor(Math.random()*200),Math.floor(Math.random()*200));
+}
+
+function squareTrans(){
+  var context = canvas.getContext('2d');
+  context.globalAlpha = Math.random();
+  context.beginPath();
+  context.arc(320, 120, 60, 0, 2 * Math.PI, false);
+  context.fillStyle = 'green';
+  context.fill();
+}
+
+document.getElementById("canvas").addEventListener("click", function(){
+  fillBlue();
+});
+
+document.getElementById("canvas").addEventListener("dblclick", function(){
+  fillRed();
+});
+
+document.getElementById("orange").addEventListener("click", function(){
+  buttonOrange();
+});
+
+document.getElementById("green").addEventListener("click", function(){
+  squareTrans();
+});
+
 drawBorder();
-fillCanvas();
