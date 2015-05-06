f3029a6b6c0d5be4c7cda3286739b4f41c405bda
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-06 10:18:13 -0600

    user can see bluerectangle

--- canvas.js
+++ canvas.js
@@ -1,6 +1,16 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+function drawBorder(){
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = { x: canvas.width, y: canvas.height };
+  context.strokeRect(0,0, size.x, size.y);
+}
+
+function fillCanvas(){
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  context.fillStyle = "blue";
+  context.fillRect(10, 10, 10, 40);
+}
+
+drawBorder();
+fillCanvas();
